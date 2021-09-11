1e266b6cb5de2782fdee50201d0ef9f0de3242e9
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2021-09-10 22:23:11 -0300

    Created data model

---
+++ src/main/java/com/backend/project/entities/Contact.java
@@ -0,0 +1,11 @@
+package com.backend.project.entities;
+
+import lombok.Data;
+
+@Data
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
